e122f7e4296eb0f7dfc44f6fd54d444d17b300f8
Sung Paik <sung.paik@codeimmersives.com>
2020-02-06 12:41:43 -0500

    created delete and update user with the postman methods

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const users = require('./models/Users');
+const users = require('./models/Users');
 // const { logMe } = require('./middleware/logger');
 // const { showMoment } = require('./middleware/moment');
+const logger = require('morgan');
 const port = process.env.PORT || '3000';
+
+// import userRoutes file from routes folder
 const userRoutes = require('./routes/userRoutes');
 
+// used for collecting information in .post route
+app.use(logger('dev'));
+
+// parses json as middleware that we can use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// connect the default path to the userRoutes in routes folder
 app.use('/api/users', userRoutes);
 // app.use(logMe);
 // app.use(showMoment);
@@ -25,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // get a single user
 // app.get('/api/users/:id', (req, res) => {
-//     const userExists = users.filter((user) => user.id === parseInt(req.params.id));
+//     const userExists = users.filter((user) => user.id === req.params.id);
 
 //     if (userExists.length !== 0) {
 //         return res.status(200).json(userExists[0]);
@@ -49,6 +58,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     newUser.email = req.body.email;
 //     users.push(newUser);
 //     return res.json(req.body);
+// });
+
+// app.put('/api/users/:id', (req, res) => {
+//     const userExists = users.filter((user) => user.id === req.params.id);
+//     const user = userExists[0];
+
+//     if (userExists.length !== 0) {
+//         const updatedUser = req.body;
+
+//         if (user.id === req.params.id) {
+//             user.name = updatedUser.name ? updatedUser.name : user.name;
+//             user.email = updatedUser.email ? updatedUser.email : user.email;
+//             return res
+//                 .status(200)
+//                 .json({ message: 'User updated.', user });
+//         }
+//     } else {
+//         return res
+//             .status(400)
+//             .json({ message: `User with id: ${req.params.id} does not exist.`});
+//     }
+// })
+
+// app.delete('/api/users/:id', (req, res) => {
+//     const userExists = users.filter((user) => user.id === req.params.id);
+//     const user = userExists[0];
+
+//     if (userExists.length !== 0) {
+//         const deletedUser = users.indexOf(user);
+//         users.splice(deletedUser, 1);
+//         return res
+//             .status(200)
+//             .json({ message: 'User Deleted', user });
+//     } else {
+//         return res
+//             .status(400)
+//             .json({ message: `User with id: ${req.params.id} does not exist.`});
+//     };
 // });
 
 app.listen(port, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    const userExists = users.filter((user) => user.id === parseInt(req.params.id));
+    const userExists = users.filter((user) => user.id === req.params.id);
 
     if (userExists.length !== 0) {
         return res.status(200).json(userExists[0]);
@@ -24,13 +24,52 @@ router.post('/', (req, res) => {
         return res
             .status(400)
             .json({ message: 'Please enter both a name and an email' });
-    }
+    };
+
     const newUser = {};
     newUser.id = uuid();
     newUser.name = req.body.name;
     newUser.email = req.body.email;
     users.push(newUser);
     return res.json(req.body);
+});
+
+router.put('/:id', (req, res) => {
+    const userExists = users.filter((user) => user.id === req.params.id);
+    const user = userExists[0];
+
+    if (userExists.length !== 0) {
+        const updatedUser = req.body;
+
+        if (user.id === req.params.id) {
+            user.name = updatedUser.name ? updatedUser.name : user.name;
+            user.email = updatedUser.email ? updatedUser.email : user.email;
+            return res
+                .status(200)
+                .json({ message: 'User updated.', user });
+        };
+    } else {
+        return res
+            .status(400)
+            .json({ message: `User with id: ${req.params.id} does not exist.`});
+    };
+});
+
+router.delete('/:id', (req, res) => {
+    const userExists = users.filter((user) => user.id === req.params.id);
+    const user = userExists[0];
+
+    if (userExists.length !== 0) {
+        const deletedUser = users.indexOf(user);
+        users.splice(deletedUser, 1);
+        return res
+            .status(200)
+            .json({ message: 'User Deleted', user });
+    } else {
+        return res
+            .status(400)
+            .json({ message: `User with id: ${req.params.id} does not exist.`});
+    };
 });
 
 module.exports = router;
